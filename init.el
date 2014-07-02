fb2a468c1db72372098ea8ab6012449fe2ef4114
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2014-07-02 21:50:22 +0200

    Remove unused stuff (mac, desktop-mode and js2-mode).

--- init.el
+++ init.el
@@ -1,27 +1,17 @@
 ;; add ~/.emacs.d/lisp to load path
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-;; add ~/.emacs.d/site-lisp to load path, if it exists
-;; used for backported code
-(let ((site-lisp (expand-file-name "site-lisp" user-emacs-directory)))
-  (when (file-directory-p site-lisp)
-    (add-to-list 'load-path site-lisp)))
-
 ;; load files
 (require 'init-package)
 (require 'init-appearance)
 (require 'init-sane-defaults)
-(when (eq system-type 'darwin)
-  (require 'init-mac))
 (require 'init-ido)
 (require 'init-server)
 (require 'init-go-mode)
 (require 'init-yaml-mode)
 (require 'init-markdown-mode)
-(require 'init-javascript)
 (require 'init-rainbow-delimiters)
 (require 'init-ibuffer)
-(require 'init-sessions)
 (require 'init-git)
 (require 'init-lisp)
 (require 'init-editing)
--- lisp/init-sessions.el
+++
@@ -1,9 +0,0 @@
-(require 'desktop)
-
-;; remember opened files
-(desktop-save-mode 1)
-
-;; save list of open files in ~/.emacs.d/.emacs.desktop
-(setq desktop-path (list user-emacs-directory))
-
-(provide 'init-sessions)
