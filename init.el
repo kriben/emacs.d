2cb849b25b3de90e8495b23ef3e6f86fdd0c8587
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2016-01-17 21:27:20 +0100

    Add c++ config.

--- init.el
+++ init.el
@@ -24,6 +24,7 @@
 (require 'init-imenu)
 (require 'init-tramp)
 (require 'init-nginx)
+(require 'init-c-mode)
 
 ;; local settings (optional)
 (require 'init-local nil t)
---
+++ lisp/init-c-mode.el
@@ -0,0 +1,23 @@
+;; install package
+(require-package 'clang-format)
+
+;; Enable company mode for c++ (will use clang backend)
+(add-hook 'c++-mode-hook 'company-mode)
+
+(defun clang-format-before-save ()
+  "Add this to .emacs to clang-format on save
+ (add-hook 'before-save-hook 'clang-format-before-save)."
+
+  (interactive)
+  (when (eq major-mode 'c++-mode) (clang-format-buffer)))
+
+
+;; Install hook to use clang-format on save
+(add-hook 'before-save-hook 'clang-format-before-save)
+
+;; Sane defaults
+(setq-default c-default-style "k&r"
+              c-basic-offset 4
+              indent-tabs-mode nil)
+
+(provide 'init-c-mode)
