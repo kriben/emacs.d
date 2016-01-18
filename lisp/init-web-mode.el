67ce92ea2e58f9a63348dea4433a3874b3e33ade
Kristian Bendiksen <kristianbendiksen@securenok.com>
2016-01-18 08:44:06 +0100

    Add jsp (ugh!) support in web-mode.

--- lisp/init-web-mode.el
+++ lisp/init-web-mode.el
@@ -9,5 +9,13 @@
 ;; use web-mode for .htm, .html and .css
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+
+;; Configuration
+(defun my-web-mode-hook()
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 4))
+
+(add-hook 'web-mode-hook 'my-web-mode-hook)
 
 (provide 'init-web-mode)
