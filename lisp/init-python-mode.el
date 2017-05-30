ec5c9d43da311cce32d5593a4eac72257aed42f6
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2017-05-30 15:42:03 +0200

    Stop guessing python indent.

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -6,6 +6,10 @@
           (lambda ()
             ;; disable electric indent
             (setq-local electric-indent-mode nil)
+            ;; Disable guessing the indentation level
+            (setq-local python-guess-indent nil)
+            (setq-local python-indent 4)
+
             ;; highlight lines longer than 79 characters (pep8)
             (setq-local fill-column 79)
             ;; use flat index in imenu
