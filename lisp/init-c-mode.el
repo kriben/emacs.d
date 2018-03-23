1e535eff73995378b3f0f79c6cac100cbfd17a30
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2018-03-23 09:15:16 +0100

    Use two-space indent for c++/c.

--- lisp/init-c-mode.el
+++ lisp/init-c-mode.el
@@ -5,9 +5,6 @@
 (add-hook 'c++-mode-hook 'company-mode)
 
 (defun clang-format-before-save ()
-  "Add this to .emacs to clang-format on save
- (add-hook 'before-save-hook 'clang-format-before-save)."
-
   (interactive)
   (when (eq major-mode 'c++-mode) (clang-format-buffer)))
 
@@ -17,7 +14,7 @@
 
 ;; Sane defaults
 (setq-default c-default-style "k&r"
-              c-basic-offset 4
+              c-basic-offset 2
               indent-tabs-mode nil)
 
 (provide 'init-c-mode)
